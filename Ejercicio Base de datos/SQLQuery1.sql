be8a0db54d689d0cfc3c0a8a556ab23676e0ee7d
Boris Hernández <borisnandez93@gmail.com>
2021-10-24 16:45:18 -0600

    cambios

---
+++ Ejercicio Base de datos/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿use [D70FFF2C19B8BD93571BAD5100E22AE6_ESTRUCTURADA\EJERCICIO BASE DE DATOS\EJERCICIO BASE DE DATOS\EJERCICIO BASE DE DATOS\TIENDA.MDF]
+
+create table clientes(
+id_cliente int, 
+nombre_cliente varchar(50),
+apellido_cliente varchar(50),
+Direccion varchar(100),
+);
+
+insert  into clientes(id_cliente,nombre_cliente,apellido_cliente,Direccion) values (1,'Carlos','Henriquez','Colonia');
+insert  into clientes(id_cliente,nombre_cliente,apellido_cliente,Direccion) values (1,'Carlos','Henriquez','Colonia');
+
+select * from clientes;
