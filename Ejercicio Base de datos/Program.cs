97e51c965c7c0181b0b6d9f05b2699f3a37c8b99
Boris Hernández <borisnandez93@gmail.com>
2021-10-24 16:37:04 -0600

    Add project files.

---
+++ Ejercicio Base de datos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Ejercicio_Base_de_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
