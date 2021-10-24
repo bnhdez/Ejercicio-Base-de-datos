ba71514acd59b9a7b08e13a647fca8f3e2124cc4
Boris Hernández <borisnandez93@gmail.com>
2021-10-24 17:40:31 -0600

    cambios

--- Ejercicio Base de datos/SQLQuery1.sql
+++ Ejercicio Base de datos/SQLQuery1.sql
@@ -1,5 +1,16 @@
 ﻿use [D70FFF2C19B8BD93571BAD5100E22AE6_ESTRUCTURADA\EJERCICIO BASE DE DATOS\EJERCICIO BASE DE DATOS\EJERCICIO BASE DE DATOS\TIENDA.MDF]
 
+create table producto(
+id_producto int,
+nombre_producto varchar(100),
+descripcion_producto varchar(200),
+precio_producto decimal,
+)
+
+insert into producto(id_producto,nombre_producto,descripcion_producto,precio_producto) values (1, 'alpina', 'botella de agua', 0.40)
+
+select * from producto;
+
 create table clientes(
 id_cliente int, 
 nombre_cliente varchar(50),
